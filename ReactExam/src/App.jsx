241bcd3d64505e729430695f3e35c3e0a8d94cd2
DeveloperRooney <han_0427@naver.com>
2022-01-31 11:10:58 +0900

    수정 기능 추가

--- ReactExam/src/App.jsx
+++ ReactExam/src/App.jsx
@@ -1,21 +1,46 @@
 import {useState} from 'react';
 
-console.clear();
-
 const App = () => {
 
+  const [editMode, setEditMode] = useState(false);
   const [text, setText] = useState("11");
+  const [newText, setNewText] = useState(text);
+  
+  const editCancel = () => {
+    setEditMode(false);
+    setNewText(text);
+  };
+
+  const commitEdit = () => {
+    setEditMode(false);
+    setText(newText);
+  }
+
+  let content = <div>
+    {text} <button onClick={() => setEditMode(true)}>수정</button>
+    </div>
+
+  if(editMode) {
+    content = (
+      <>
+        <input
+          type="text"
+          value={newText}
+          onChange={({ target: { value } }) => {
+            setNewText(value);
+          }}
+        />
+        <button onClick={() => editCancel()}>수정 취소</button>
+        <button onClick={() => commitEdit()}>수정</button>
+      </>
+    );
+  }
 
   return (
-    <div classNmae="App">
-      <input type="text"
-        onChange={({target : { value }}) => {
-          setText(value);
-        }}
-        value={text}
-      />
-    </div>
-  );
+  <div className='App'>
+    {content}
+  </div>
+  )
 }
 
 export default App;
