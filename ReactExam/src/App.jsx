f51e8bb2375ddf0f323bb5115b79cc023bc2cd1e
DeveloperRooney <han_0427@naver.com>
2022-01-31 17:39:02 +0900

    To Do List 리스트 작성

--- ReactExam/src/App.jsx
+++ ReactExam/src/App.jsx
@@ -1,10 +1,53 @@
 import {useState} from 'react';
+import "./main.css";
 
 const App = () => {
 
+  console.clear();
+
+  const [newToDo, setNewToDO] = useState("");
+  const [toDoList, setToDoList] = useState([]);
+  const [toDoIndex, setToDoIndex] = useState(0);
+
+  const addToDo = () => {
+
+    const temp = {
+      id : toDoIndex + 1,
+      title : newToDo
+    };
+
+    setToDoIndex(toDoIndex + 1);
+
+    setToDoList([...toDoList, temp]);
+    setNewToDO("");
+  }
+
+  let showToDoList = <div>등록된 리스트가 없습니다.</div>
+
+  if(toDoList.length != 0) {
+    showToDoList = <ul>
+      {toDoList.map((toDo)=> {
+        return <li key={toDo.id}>{toDo.id} {toDo.title}</li>
+      })}
+    </ul>
+  }
+
   return (
-    <></>
-  )
+    <div className="layout">
+      <div className='top-menu'>
+        <span className='logo'>To Do List</span>
+        <input type="text"
+          value={newToDo}
+          onChange={({target : {value}}) => {
+            setNewToDO(value);
+          }}
+        />
+        <button onClick={() => addToDo()}>등록</button>
+      </div>
+      {showToDoList}
+    </div>
+  );
+
 }
 
 export default App;
