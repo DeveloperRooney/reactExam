9074a43e3be097d5b7dfd4df63999c367e48b3a5
DeveloperRooney <han_0427@naver.com>
2022-02-02 14:39:01 +0900

    컴포넌트 분리 연습을 위한 초기화

--- ReactExam/src/App.jsx
+++ ReactExam/src/App.jsx
@@ -2,62 +2,10 @@ import {useState} from 'react';
 import "./main.css";
 
 const App = () => {
-
-  console.clear();
-
-  const [newToDo, setNewToDO] = useState("");
-  const [toDoList, setToDoList] = useState([]);
-  const [toDoIndex, setToDoIndex] = useState(0);
-
-  const addToDo = () => {
-
-    const temp = {
-      id : toDoIndex + 1,
-      title : newToDo
-    };
-
-    setToDoIndex(toDoIndex + 1);
-
-    setToDoList([...toDoList, temp]);
-    setNewToDO("");
-  }
-
-  const toDoDelete = (id) => {
-    setToDoList(toDoList.filter(todo => todo.id != id ))
-  }
-
-  let showToDoList = <div>등록된 리스트가 없습니다.</div>
-
-  if(toDoList.length != 0) {
-    showToDoList = <ul>
-      {toDoList.slice(0).reverse().map((toDo)=> {
-        return (
-          <li key={toDo.id}>
-            {toDo.id} {toDo.title}
-            &nbsp;<button onClick={() => toDoDelete(toDo.id)}>삭제</button>
-            &nbsp;<button>수정</button>
-          </li>
-        );
-      })}
-    </ul>
-  }
-
   return (
-    <div className="layout">
-      <div className='top-menu'>
-        <span className='logo'>To Do List</span>
-        <input type="text"
-          value={newToDo}
-          onChange={({target : {value}}) => {
-            setNewToDO(value);
-          }}
-        />
-        <button onClick={() => addToDo()}>등록</button>
-      </div>
-      {showToDoList}
-    </div>
-  );
-
+    <>
+      테스트
+    </>
+  )
 }
-
 export default App;
